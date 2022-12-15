19e29b79b3e3425c5b515997becdb05622e311e1
Dino <radonjicdino@gmail.com>
2022-12-15 20:38:20 +0100

    Tuning Trouble

---
+++ Day 06.cs
@@ -0,0 +1,34 @@
+﻿var reader = new StreamReader(@"C:\\Users\\radon\\Desktop\\input.txt");
+int counter = 14;
+List<char> list = new List<char>
+{
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read(),
+    (char)reader.Read()
+};
+
+char x = (char)reader.Read();
+while (x >= 'a' && x <= 'z')
+{
+    List<char> temp = list.Distinct().ToList();
+    if (temp.Count == 14)
+    {
+        break;
+    }
+    list.RemoveAt(0);
+    list.Add(x);
+    counter++;
+    x = (char)reader.Read();
+}
+Console.WriteLine(counter);
