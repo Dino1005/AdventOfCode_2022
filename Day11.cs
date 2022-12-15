774da93c6553e0063fc7be58a1a181249c3f093d
Dino <radonjicdino@gmail.com>
2022-12-15 20:40:14 +0100

    Monkey in the Middle

---
+++ Day11.cs
@@ -0,0 +1,136 @@
+﻿using System.Drawing;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+var reader = new StreamReader(@"C:\\Users\\radon\\Desktop\\input.txt"); 
+string line = reader.ReadLine();
+
+int x = 0, div = 1;
+string items = null, operation = null, test = null, lcm = null;
+List<Monkey> monkeys = new List<Monkey>();
+
+while (line != null)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        string[] temp = line.Split();
+        if (i == 1)
+        {
+
+            for (int j = 4; j < temp.Length; j++)
+            {
+                items += temp[j];
+            }
+        }
+        if (i == 2)
+        {
+            operation += temp[temp.Length - 2] + ',' + temp[temp.Length - 1];
+        }
+        if (i == 3)
+        {
+            test += temp[temp.Length - 1];
+            lcm += temp[temp.Length - 1] + ',';
+            line = reader.ReadLine();
+            string[] ifTrue = line.Split();
+            test += ',' + ifTrue[ifTrue.Length - 1];
+            line = reader.ReadLine();
+            string[] ifFalse = line.Split();
+            test += ',' + ifFalse[ifFalse.Length - 1];
+        }
+
+        line = reader.ReadLine();
+    }
+    monkeys.Add(new Monkey(items, operation, test));
+    items = null;
+    operation = null;
+    test = null;
+    line = reader.ReadLine();
+}
+
+foreach (var monkey in monkeys)
+{
+    div *= int.Parse(monkey.test.Split(",").First());
+}
+
+for (int i = 0; i < 10000; i++)
+{
+    foreach (var monkey in monkeys)
+    {
+        while(monkey.itemWorryLevel.Count != 0)
+        {
+            monkey.DoOperation();
+            monkey.count++;
+            monkey.itemWorryLevel[0] = monkey.itemWorryLevel[0] % div;
+            x = monkey.Test();
+            monkeys[x].itemWorryLevel.Add(Monkey.item);
+        }
+        
+    }
+}
+Monkey first = monkeys.MaxBy(x => x.count);
+monkeys.Remove(first);
+Monkey second = monkeys.MaxBy(x => x.count);
+Console.WriteLine(first.count);
+Console.WriteLine(second.count);
+Console.WriteLine(first.count * second.count);
+
+public class Monkey
+{
+    public List<long> itemWorryLevel = new List<long>();
+    public string operation;
+    public string test;
+    public static long item;
+    public long count;
+
+    public Monkey(string items, string operation, string test)
+    {
+        foreach (var item in items.Split(","))
+        {
+            itemWorryLevel.Add(long.Parse(item));
+        }
+        this.operation = operation;
+        this.test = test;
+        count = 0;
+    }
+
+    public void DoOperation()
+    {
+        string[] split = operation.Split(",");
+        if (split[0] == "+")
+        {
+            if (long.TryParse(split[1], out long number))
+            {
+                itemWorryLevel[0] += number;
+            }
+            else
+                itemWorryLevel[0] += itemWorryLevel[0];
+        }
+        else
+        {
+            if (long.TryParse(split[1], out long number))
+            {
+                itemWorryLevel[0] *= number;
+            }
+            else
+                itemWorryLevel[0] *= itemWorryLevel[0];
+        }
+    }
+
+    public int Test()
+    {
+        string[] split = test.Split(",");
+        int ifFalse = int.Parse(split[2]);
+        int ifTrue = int.Parse(split[1]);
+
+        if (itemWorryLevel[0] % int.Parse(split[0]) == 0)
+        {
+            item = itemWorryLevel[0];
+            itemWorryLevel.RemoveAt(0);
+            return ifTrue;
+        }
+        item = itemWorryLevel[0];
+        itemWorryLevel.RemoveAt(0);
+        return ifFalse;
+    }
+}
