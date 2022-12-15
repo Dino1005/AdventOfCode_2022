c0e68d163271e8bdeb7b845f5f1b3e7d7bbcac97
Dino <radonjicdino@gmail.com>
2022-12-15 20:36:05 +0100

    Rock Paper Scissors

---
+++ Day 02.cs
@@ -0,0 +1,72 @@
+﻿var reader = new StreamReader(@"C:\\Users\\radon\\Desktop\\input.txt");
+int i = 0, points = 0;
+string line = reader.ReadLine();
+
+//while (line != null)
+//{
+//    if(line.First() == 'A')
+//    {
+//        if (line.Last() == 'X')
+//            points += 4;
+//        else if (line.Last() == 'Y')
+//            points += 8;
+//        else if (line.Last() == 'Z')
+//            points += 3;
+//    }
+
+//    else if (line.First() == 'B')
+//    {
+//        if (line.Last() == 'X')
+//            points += 1;
+//        else if (line.Last() == 'Y')
+//            points += 5;
+//        else if (line.Last() == 'Z')
+//            points += 9;
+//    }
+
+//    else if (line.First() == 'C')
+//    {
+//        if (line.Last() == 'X')
+//            points += 7;
+//        else if (line.Last() == 'Y')
+//            points += 2;
+//        else if (line.Last() == 'Z')
+//            points += 6;
+//    }
+//    line = reader.ReadLine();
+//}
+
+while (line != null)
+{
+    if (line.First() == 'A')
+    {
+        if (line.Last() == 'X')
+            points += 3;
+        else if (line.Last() == 'Y')
+            points += 4;
+        else if (line.Last() == 'Z')
+            points += 8;
+    }
+
+    else if (line.First() == 'B')
+    {
+        if (line.Last() == 'X')
+            points += 1;
+        else if (line.Last() == 'Y')
+            points += 5;
+        else if (line.Last() == 'Z')
+            points += 9;
+    }
+
+    else if (line.First() == 'C')
+    {
+        if (line.Last() == 'X')
+            points += 2;
+        else if (line.Last() == 'Y')
+            points += 6;
+        else if (line.Last() == 'Z')
+            points += 7;
+    }
+    line = reader.ReadLine();
+}
+Console.WriteLine(points);
